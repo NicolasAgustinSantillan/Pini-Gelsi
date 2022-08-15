d23629fcda86ba4d67cacb820ddaf444728f5e0e
Nicolas Santillan <panuelo81@outlook.com.ar>
2022-08-15 20:20:14 -0300

    update (capa modelo): plan cuenta

---
+++ 010_VENTAS_WEB_Proyecto/CapaModelo/PlanCuenta.cs
@@ -0,0 +1,14 @@
+﻿using Syusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo
+{
+    public class PlanCuenta
+    {
+        public int PlanCuentasId { get; set; }
+        public string Tipo { get; set; }
+    }
+}
---
+++ 010_VENTAS_WEB_Proyecto/CapaModelo/PlanCuentasRubros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo
+{
+    public class PlanCuentasRubros
+    {
+        // VISTA
+        public string Numero { get; set; }
+        public string PlanCuentas { get; set; }
+        public string Rubro { get; set; }
+        public string Cuenta { get; set; }
+    }
+}
